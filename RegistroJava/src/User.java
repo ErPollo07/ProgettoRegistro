9c4c8930c19e4c907f7e871aa2281a76c5a7f20d
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-02-28 15:24:14 +0100

    Add the getPassword method and change the other

--- RegistroJava/src/User.java
+++ RegistroJava/src/User.java
@@ -1,17 +1,18 @@
 public class User {
 
     /**
-     * Set a new name for the user
+     * Set a new name for the user with id equals to the first param
      * Es - My name is "nicola" in file User.json, if I call the function
      * and I put the parameter "giancarlo", my name now will be "giancarlo"
      * @param name new name for the user
      */
-    public void setName(String name) {
+    public void setName(String name, String id) {
     }
 
     /**
      * The method gives the name of the user with id equals to the first param.
-     * Because a user doesn't have the permission to look at the settings of another account
+     * Check if the userId is an id of an admin,
+     * because only the admin can view all the information about all the users
      * @param id The of the user who we want to get the name
      * @param userId The id of the current user
      * @return returns the name of the user with id equals to the first param
@@ -21,19 +22,17 @@ public class User {
     }
 
     /**
-     * Set a new password
+     * Set pw as new password to the user with the id equals to the first param
      * @param pw new password
      */
-    public void setPassword(String pw) {
+    public void setPassword(String pw, String id) {
     }
 
     /**
      * Get the password of the user with the id equals to first param
-     * Check if the userId is an id of an admin.
-     * If the id isn't an id of an admin check if the id and the userId (both the param) is equal,
-     * if the condition is true return the password
-     * @param id
-     * @param userId
+     * Check if the id correspond to userId because a user can't view the password of another user
+     * @param id the id of the user who we want to get the password
+     * @param userId the id of the current user
      * @return retun the password if
      */
     public String getPassword(String id, String userId) {
