893c5d62dfc760c85bedc8f7f15efba3e243601d
Denis0160 <denis.sescu@iisviolamarchesini.edu.it>
2024-03-01 12:55:30 +0100

    added metods of administrator

--- RegistroJava/src/Administrator.java
+++ RegistroJava/src/Administrator.java
@@ -1,3 +1,68 @@
 public class Administrator {
 
+    /***
+     * viewClass: visualizza elenco degli studenti di una classe
+     * viewTeacher: visualizza elenco degli insegnanti
+     * setGrade: permette di inserire i voti a un singolo studente.
+     * viewGrade: permette di vedere i voti di uno studente.
+     * insertLessons: permette di inserire gli argomenti delle lezioni.
+     * viewLessosns: permette di visualizzare i le lezioni svolte.
+     * viewNoticeBoard: metodo che consente di visualizzare la bacheca.
+     * viewEvents: metodo che consente di visualizzare gli eventi in programma.
+     * viewAgenda: permette di visualizzare i contenuti dell’agenda
+     * setAgendaNote: permette di inserire compiti, verifiche.
+     * viewDocuments: metodo che consente di visualizzare i documenti.
+     * viewSchoolReport: permette di visualizzare le pagelle
+     * viewFiles: Visualizzazione dei file all’interno della bacheca.
+     * @return
+     */
+
+    public String[] getClassStudent(){
+        return new String[1];
+    }
+    public String[] getTeacher(){
+        return new String[1];
+    }
+
+    public double[] setGrade(){
+        return new double[1];
+    }
+
+    public double[] getGrade(){
+        return new double[1];
+    }
+
+    public String[] setLessons(){
+        return new String[1];
+    }
+
+    public String[] getLessons(){
+        return new String[1];
+    }
+
+    public String[] getNoticeBoard(){
+        return new String[1];
+    }
+    public String[] getEvents(){
+        return new String[1];
+    }
+    public String[] setAgenda(){
+        return new String[1];
+    }
+    public String[] getAgenda(){
+        return new String[1];
+    }
+    public String[] getDocuments(){
+        return new String[1];
+    }
+
+    public String[] getSchoolReport(){
+        return new String[1];
+    }
+    public String[] getFiles(){
+        return new String[1];
+    }
+
+
 }
+
