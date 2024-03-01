0e044cf212788d240c12500ff889db907e471de4
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-03-01 15:31:05 +0100

    add methods to Teacher.java and Student.java add setNoteToStudent, getAgendaNote and setAgendaNote on Teacher.java file add getLesson and getGrades on Student.java file

--- RegistroJava/src/Administrator.java
+++ RegistroJava/src/Administrator.java
@@ -1,99 +1,95 @@
-public class Administrator {
-    /***
+public class Administrator extends User {
+
+    /**
      * these are the methods mainly used by the administrator
      * @return
      */
-
-
     public String[] getClassStudent(){
         return new String[1];
-        /***
-         * This method allows you to view the first and last names of a class of students
-         */
     }
+
+    /**
+     * This method allows you to view the teachers belonging to a class
+     */
     public String[] getTeacher(){
         return new String[1];
-        /***
-         * This method allows you to view the teachers belonging to a class
-         */
     }
 
+    /**
+     * allows you to enter grades for a single student.
+     */
     public double[] setGrade(){
         return new double[1];
-        /***
-         * allows you to enter grades for a single student.
-         */
-
     }
 
+    /**
+     * viewGrade: allows you to see a student's grades.
+     */
     public double[] getGrade(){
         return new double[1];
-        /***
-         * viewGrade: allows you to see a student's grades.
-         */
-
     }
 
+    /**
+     * method that allows us to insert lessons
+     */
     public String[] setLessons(){
         return new String[1];
-        /***
-         * method that allows us to insert lessons
-         */
     }
 
+    /**
+     *  allows you to view the lessons completed.
+     */
     public String[] getLessons(){
         return new String[1];
-        /***
-         *  allows you to view the lessons completed.
-         */
     }
 
+    /**
+     *  method that allows you to view the notice board.
+     */
     public String[] getNoticeBoard(){
         return new String[1];
-        /***
-         *  method that allows you to view the notice board.
-         */
     }
+
+    /**
+     *  method that allows you to view scheduled events.
+     */
     public String[] getEvents(){
         return new String[1];
-        /***
-         *  method that allows you to view scheduled events.
-         */
-
     }
+
+    /**
+     *  setAgendaNote: allows you to insert tasks and checks.
+     */
     public String[] setAgenda(){
         return new String[1];
-        /***
-         *  setAgendaNote: allows you to insert tasks and checks.
-         */
     }
+
+    /**
+     *  allows you to view the contents of the agenda
+     */
     public String[] getAgenda(){
         return new String[1];
-        /***
-         *  allows you to view the contents of the agenda
-         */
     }
+
+    /**
+     *  method that allows you to view documents.
+     */
     public String[] getDocuments(){
         return new String[1];
-        /***
-         *  method that allows you to view documents.
-         */
     }
 
+    /**
+     * allows you to view report cards
+     */
     public String[] getSchoolReport(){
         return new String[1];
-        /***
-         * allows you to view report cards
-         */
     }
 
+    /**
+     * View files within the board.
+     */
     public String[] getFiles(){
         return new String[1];
-        /***
-         * View files within the board.
-         */
     }
-
-
 }
 
--- RegistroJava/src/Student.java
+++ RegistroJava/src/Student.java
@@ -1,3 +1,24 @@
 public class Student extends User {
 
+    /**
+     * If I put the studentId = "S000001" and subject = "math"
+     * the method returns all the grades of the student, with the id = studentId, in the specified subject
+     * @param studentId the ID of the student whose grades you want to see
+     * @param subject the subject from which the votes are to be obtained
+     * @return the grades of the specified student in the specified subject
+     */
+    public int[] getGrades(String studentId, String subject) {
+        return new int[1];
+    }
+
+    /**
+     * If I put the classroom = "3E" e date = "20/2/2024" the method return all the lessons of the 3E in date 20/2/2024
+     *
+     * @param classroom the student's class
+     * @param date the date you want to know the lessons
+     * @return all the lessons in a certain classroom in the specified date
+     */
+    public String[][] getLesson(String classroom, String date) {
+        return new String[1][1];
+    }
 }
