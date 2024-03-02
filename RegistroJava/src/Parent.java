0235beafbe42d61624ed440fb24a26cd172a1b18
67lori898 <lorenzo.malachin@iisviolamarchesini.edu.it>
2024-03-02 16:16:30 +0100

    added new methods to the parent.java class: setMeeting, Admi

--- RegistroJava/src/Parent.java
+++ RegistroJava/src/Parent.java
@@ -1,3 +1,28 @@
-public class Parent {
+public class Parent extends User{
+
+
+// per formattare il codice bene premere ctrl+alt+l
+
+
+    public String[] setMeetings() {
+        return new String[1];    /
+        /***
+         *  allows you to book a meeting with the teachers.
+         */
+    }
+
+    public String[] Administration() {
+        return new String[1];
+        /***
+         *  method that allows you to view the absence and the presence of the student.
+         */
+    }
+
+    public String[] justifyAbsences() {
+        return new String[1];
+        /***
+         * allows the parent to justify the absence of the student.
+         */
+    }
 
 }
