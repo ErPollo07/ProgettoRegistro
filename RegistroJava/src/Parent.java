5cf529a665f2dbc4a284f252ac420fe49f0a2bec
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-03-03 11:20:08 +0100

    Move the comments on Parent.java class

--- RegistroJava/src/Parent.java
+++ RegistroJava/src/Parent.java
@@ -4,25 +4,25 @@ public class Parent extends User{
 // per formattare il codice bene premere ctrl+alt+l
 
 
+    /**
+     *  allows you to book a meeting with the teachers.
+     */
     public String[] setMeetings() {
-        return new String[1];    /
-        /***
-         *  allows you to book a meeting with the teachers.
-         */
+        return new String[1];
     }
 
+    /**
+     *  method that allows you to view the absence and the presence of the student.
+     */
     public String[] Administration() {
         return new String[1];
-        /***
-         *  method that allows you to view the absence and the presence of the student.
-         */
     }
 
+    /**
+     * allows the parent to justify the absence of the student.
+     */
     public String[] justifyAbsences() {
         return new String[1];
-        /***
-         * allows the parent to justify the absence of the student.
-         */
     }
 
 }
