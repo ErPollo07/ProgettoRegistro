5e2f18d7cbb589529cbd03f569df9d516e259a68
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-03-13 17:34:51 +0100

    Add comment and part of the code in access method in Main, add readJsonFile and verifyAccess methods

--- RegistroJava/src/Main.java
+++ RegistroJava/src/Main.java
@@ -1,12 +1,20 @@
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+
+    static JSONObject userJson = new JSONObject();
+    static JSONObject agendaJson = new JSONObject();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        JSONObject userJson = new JSONObject();
-        JSONObject agendaJson = new JSONObject();
 
         String[] accessMenu = {
                 "Cosa vuoi fare?",
@@ -37,12 +45,55 @@ public class Main {
      * @param scanner scanner
      */
     private static void access(Scanner scanner) {
+        String id;
+        String password;
+        String filePath = "";
+
         System.out.println("===Accedi===");
         System.out.println("Inserisci il codice personale o l'username della scuola: ");
-        System.out.println("->\t");
+        System.out.println("-> ");
+
+        id = scanner.nextLine();
 
         System.out.println("Inserisci la password:\n ");
 
+        password = scanner.nextLine();
+
+        // Check what type of user is
+        if (id.charAt(0) == 'S') {
+            filePath = "JSON/JsonFile/User/Students.json";
+            readJsonFile(filePath);
+        }
+
+        // Search in the file of the user type
+        // if there is a user with id equal to what the user putted and the password equal to what the user putted
+        verifyAccess();
+
+
+        // if there isn't a user with the id or password equal to what the user putted
+    }
+
+    /**
+     * Search in the file of the user type
+     * if there is a user with id equal to what the user putted and the password equal to what the user putted.
+     */
+    private static void verifyAccess() {
+
+    }
+
+    private static void readJsonFile(String fileName) {
+        // Create the json parser
+        JSONParser jsonParser = new JSONParser();
+
+        // Read and save the Agenda.json content in jsonObj
+        try (FileReader fileReader = new FileReader(fileName)) {
+            Object obj = jsonParser.parse(fileReader);
+            // assign to the JSONObject (global variable)
+            // the file which we want ot extract information from
+            userJson = (JSONObject) obj;
+        } catch (IOException | ParseException e) {
+            System.out.println("C'e' stato un problema con i file di accesso. Ci scusiamo per il disagio");
+        }
     }
 
     /**
